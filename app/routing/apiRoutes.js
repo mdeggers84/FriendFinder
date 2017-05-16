b804dedabd06de21bd05fb6c5a3d52e9d802ba0f
Michael Eggers <mdeggers84@gmail.com>
2017-05-16 10:57:44 -0400

    minor updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,17 +9,7 @@ var apiRoutes = function (app, bodyParser) {
   app.use(bodyParser.text());
   app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-  // // displays current list of json objects
-  // app.get('/api/friends', function (req, res) {
-  //   return res.json(userArr);
-  // });
-
-  // // pushes new user content to arr
-  // app.post('/api/friends', function (req, res) {
-  //   var newUser = req.body;
-  //   userArr.push(newUser);
-  // });
-    // displays current list of json objects
+  // displays current list of json objects
   app.get('/api/friends', function (req, res) {
     var userArr = JSON.parse(fs.readFileSync('app/data/users.json', 'utf8'));
     return res.json(userArr);
@@ -31,7 +21,7 @@ var apiRoutes = function (app, bodyParser) {
     var userArr = JSON.parse(fs.readFileSync('app/data/users.json', 'utf8'));
 
     userArr.push(newUser);
-    fs.writeFileSync('app/data/users.json', JSON.stringify(userArr));
+    fs.writeFile('app/data/users.json', JSON.stringify(userArr));
     res.json(newUser);
   });
 };
